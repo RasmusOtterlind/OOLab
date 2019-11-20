9db2b6e0e9770da74d1ee4ded2d31594392283cd
961028 <emilcarlholmsten@gmail.com>
2019-11-20 13:30:04 +0100

    Started working on Storage class

--- src/OOCar/Storage.java
+++ src/OOCar/Storage.java
@@ -3,7 +3,7 @@ package OOCar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Storage {
+class Storage {
 
     enum UnloadOrder {
         firstInFirstOut, lastInFirstOut, selected;
