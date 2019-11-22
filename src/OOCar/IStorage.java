f0efd0bb0b9ec7c7b964cb8558dc71690986f39a
RasmusOtterlind <57478279+RasmusOtterlind@users.noreply.github.com>
2019-11-22 15:29:17 +0100

    Flatbed if statements correction

--- src/OOCar/IStorage.java
+++ src/OOCar/IStorage.java
@@ -1,7 +1,19 @@
 package OOCar;
 
+/**
+ * An interface to help use Polymorphism when moving loaded vehicles
+ * @param <T>
+ */
 interface IStorage<T extends Vehicle> {
+    /**
+     * method that moves the given vehicles storage
+     */
     void moveStorage();
+
+    /**
+     * adds specific vehicle to storage
+     * @param vehicle
+     */
     void addToStorage(T vehicle);
     Vehicle unloadStorage();
 }
