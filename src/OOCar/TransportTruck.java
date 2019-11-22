256c6a6ad557b3809a2a14cb001ebe11f5e48517
salvija <salvija.zelvyte22@gmail.com>
2019-11-22 11:45:20 +0100

    hejhej

--- src/OOCar/TransportTruck.java
+++ src/OOCar/TransportTruck.java
@@ -2,14 +2,27 @@ package OOCar;
 
 import java.awt.*;
 
+/**
+ * A subclass to Vehicle. Implements interface IStorage.
+ */
 public class TransportTruck extends Vehicle implements IFlatbed, IStorage {
+    /**
+     *  Composition: Creating new object flatbed to reach method in the class Flatbed with the help of delegation.
+     */
     private Flatbed flatbed = new Flatbed(100, 100, 0);
+    /**
+     *  Composition: Creating new object storage to reach method in the class Storage with the help of delegation.
+     */
     private Storage<Vehicle> storage = new Storage<>(10, Storage.UnloadOrder.firstInFirstOut, 3.0);
 
     TransportTruck() {
         super(2, 200, 0, Color.magenta, "TransportTruck-2000", 0.05,10);
     }
 
+    /**
+     * Add vehicle to the storage
+     * @param vehicle The vehicle you want to load.
+     */
     public void addToStorage(Vehicle vehicle){
         storage.addToStorage(vehicle,getX(),getY(),getDirection());
     }
