0555ddb26b65db1f0cd242f2ea00c3774001fd14
RasmusOtterlind <57478279+RasmusOtterlind@users.noreply.github.com>
2019-12-11 14:47:57 +0100

    Lab 2 Part b Beginning of refactoring plan. Created model package, new interfaces and removed dependencies with the factory method design pattern.

--- src/CarController.java
+++ src/CarController.java
@@ -1,6 +1,7 @@
 import model.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -25,31 +26,39 @@ public class CarController {
     private JButton stopButton = new JButton("Stop all cars");
     private ArrayList<JButton> buttonsList = new ArrayList<>();
     // member fields
-
     // The delay (ms) corresponds to 20 updates a sec (hz)
-    private final int delay = 13;
+    //private final int delay = 13;
     // The timer is started with an listener (see below) that executes the statements
     // each step between delays.
-    private Timer timer = new Timer(delay, new TimerListener());
-
+    //private Timer timer = new Timer(delay, new TimerListener());
     // The frame that represents this instance View of the MVC pattern
     private CarView frame;
     // A list of cars, modify if needed
-    private ArrayList<IVehicle> vehicles = new ArrayList<>();
-
+    private CarModel model = new CarModel();
     public CarController() {
         buttonsList.add(gasButton);
         buttonsList.add(brakeButton);
         buttonsList.add(turboOnButton);
-        //buttonsList.add(startButton);
         buttonsList.add(turboOffButton);
         buttonsList.add(liftBedButton);
         buttonsList.add(lowerBedButton);
-        //buttonsList.add(stopButton);
+        gasButton.addActionListener(e -> model.gas(frame.getGasAmount()));
+        startButton.addActionListener(e -> model.startCars());
+        brakeButton.addActionListener(e -> model.brake(frame.getGasAmount()));
+        turboOnButton.addActionListener(e ->model.turboOn());
+        turboOffButton.addActionListener(e -> model.turboOff());
+        liftBedButton.addActionListener(e -> model.liftBed());
+        lowerBedButton.addActionListener(e ->model.lowerBed());
+        stopButton.addActionListener(e -> model.stopCars());
+        frame = new CarView("hej",buttonsList);
+        stopButton.setPreferredSize(new Dimension(frame.getWidth()/5-15,200));
+        startButton.setPreferredSize(new Dimension(frame.getWidth()/5-15,200));
+        frame.add(startButton);
+        frame.add(stopButton);
     }
 
 //methods:
-
+/*
     public static void main(String[] args) {
         // Instance of this class
         CarController cc = new CarController();
@@ -59,25 +68,19 @@ public class CarController {
         cc.vehicles.add(VehicleFactory.createScaniaTruck(100,300,0));
 
         // Start a new view and send a reference of self
-        cc.frame = new CarView("CarSim 1.0",cc.buttonsList);
-        cc.frame.add(cc.startButton);
-        cc.frame.add(cc.stopButton);
-        cc.gasButton.addActionListener(e -> cc.gas(cc.frame.getGasAmount()));
-        cc.startButton.addActionListener(e -> cc.startCars());
-        cc.brakeButton.addActionListener(e -> cc.brake(cc.frame.getGasAmount()));
-        cc.turboOnButton.addActionListener(e ->cc.turboOn());
-        cc.turboOffButton.addActionListener(e -> cc.turboOff());
-        cc.liftBedButton.addActionListener(e -> cc.liftBed());
-        cc.lowerBedButton.addActionListener(e ->cc.lowerBed());
-        cc.stopButton.addActionListener(e -> cc.stopCars());
+        //cc.frame = new CarView("CarSim 1.0",cc.buttonsList);
+
+
 
         // Start the timer
         cc.timer.start();
     }
 
+ */
+
     /* Each step the TimerListener moves all the cars in the list and tells the
     * view to update its images. Change this method to your needs.
-    * */
+    * *//*
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             for (IVehicle car : vehicles){
@@ -91,71 +94,9 @@ public class CarController {
             frame.drawPanel.moveIt(vehicles);
         }
     }
+    */
 
     // Calls the gas method for each car once
-    private void gas(int amount) {
-        double gas = ((double) amount) / 100;
-        for (IVehicle car : vehicles
-                ) {
-            car.gas(gas);
-        }
-    }
-    private void startCars(){
-        for(IVehicle car : vehicles){
-            car.startEngine();
-        }
-    }
-   private void brake(int amount){
-       double brake = ((double) amount) / 100;
-        for (IVehicle car : vehicles){
-            car.brake(brake);
-        }
-   }
-   private void turboOn(){
-        for(IVehicle vehicle : vehicles) {
-            if (vehicle instanceof ITurbo) {
-                ((ITurbo)vehicle).setTurboOn();
-            }
-        }
-   }
-   private void turboOff(){
-       for(IVehicle vehicle : vehicles) {
-           if (vehicle instanceof ITurbo) {
-               ((ITurbo)vehicle).setTurboOff();
-           }
-       }
-   }
-   private void liftBed(){
-        for(IVehicle vehicle : vehicles){
-            if (vehicle instanceof IFlatBed) {
-                ((IFlatBed)vehicle).raiseFlatBed();
-            }
-        }
-   }
-    private void lowerBed(){
-        for(IVehicle vehicle : vehicles){
-            if (vehicle instanceof IFlatBed) {
-                ((IFlatBed)vehicle).lowerFlatBed();
-            }
-        }
-    }
-    private void stopCars(){
-        for(IVehicle vehicle : vehicles){
-            vehicle.stopEngine();
-        }
 
-    }
-
-   private void changeDirectionIfOut(IVehicle vehicle){
-        if(checkIfOut(vehicle)){
-            vehicle.setDirection(vehicle.getDirection()+Math.PI);
-        }
-   }
-   private boolean checkIfOut(IVehicle entity){
-        if((entity.getX()<=0|| entity.getX()>=700) || entity.getY()<=30 || entity.getY()>=500){
-            return true;
-        }
-        return false;
-   }
 
 }
--- src/CarModel.java
+++ src/CarModel.java
@@ -1,2 +1,79 @@
+import model.IFlatBed;
+import model.ITurbo;
+import model.IVehicle;
+
+import java.util.ArrayList;
+
 public class CarModel {
+    private ArrayList<IVehicle> vehicles = new ArrayList<>();
+
+
+
+
+
+
+    public void gas(int amount) {
+        double gas = ((double) amount) / 100;
+        for (IVehicle car : vehicles
+        ) {
+            car.gas(gas);
+        }
+    }
+    public void startCars(){
+        for(IVehicle car : vehicles){
+            car.startEngine();
+        }
+    }
+    public void brake(int amount){
+        double brake = ((double) amount) / 100;
+        for (IVehicle car : vehicles){
+            car.brake(brake);
+        }
+    }
+    public void turboOn(){
+        for(IVehicle vehicle : vehicles) {
+            if (vehicle instanceof ITurbo) {
+                ((ITurbo)vehicle).setTurboOn();
+            }
+        }
+    }
+    public void turboOff(){
+        for(IVehicle vehicle : vehicles) {
+            if (vehicle instanceof ITurbo) {
+                ((ITurbo)vehicle).setTurboOff();
+            }
+        }
+    }
+    public void liftBed(){
+        for(IVehicle vehicle : vehicles){
+            if (vehicle instanceof IFlatBed) {
+                ((IFlatBed)vehicle).raiseFlatBed();
+            }
+        }
+    }
+    public void lowerBed(){
+        for(IVehicle vehicle : vehicles){
+            if (vehicle instanceof IFlatBed) {
+                ((IFlatBed)vehicle).lowerFlatBed();
+            }
+        }
+    }
+    public void stopCars(){
+        for(IVehicle vehicle : vehicles){
+            vehicle.stopEngine();
+        }
+
+    }
+
+    public void changeDirectionIfOut(IVehicle vehicle){
+        if(checkIfOut(vehicle)){
+            vehicle.setDirection(vehicle.getDirection()+Math.PI);
+        }
+    }
+    private boolean checkIfOut(IVehicle entity){
+        if((entity.getX()<=0|| entity.getX()>=700) || entity.getY()<=30 || entity.getY()>=500){
+            return true;
+        }
+        return false;
+    }
 }
